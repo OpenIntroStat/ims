e7d0267671e2f713b8fad175291ea94989c04844
Jo Hardin <jo.hardin@pomona.edu>
2020-08-11 19:36:20 -0700

    I had to add the gridExtra package, not really sure how to do it!

--- _common.R
+++ _common.R
@@ -32,6 +32,7 @@ suppressMessages(library(infer))
 suppressMessages(library(patchwork))
 suppressMessages(library(gghighlight))
 suppressMessages(library(maps))
+suppressMessages(library(gridExtra))
 
 # dplyr options ----------------------------------------------------------------
 
